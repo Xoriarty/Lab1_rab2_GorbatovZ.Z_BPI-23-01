9818af25f034ef592733184d58a19da2a1331ce3
Xoriarty <xoriartycode@gmail.com>
2025-09-22 11:27:02 +0500

    Создание базового класса и класса Ромб

---
+++ Lab1_rab2_GorbatovZ.Z_BPI-23-01/Classes/Figure.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+
+namespace Lab1_rab2_GorbatovZ.Z_BPI_23_01.Classes
+{
+    public abstract class Figure
+    {
+        private string _name;
+        public Figure(string name)
+        {
+            Name = name;
+            Console.WriteLine($"Создана фигура: {name}");
+        }
+        public string Name
+        {
+            get { return _name; }
+            set
+            { _name = value; }
+        }
+
+        public virtual double CalculateArea()
+        {
+            Console.WriteLine("Вычисление площади в базовом классе");
+            return 0;
+        }
+
+        public virtual double CalculatePerimeter()
+        {
+            Console.WriteLine("Вычисление периметра в базовом классе");
+            return 0;
+        }
+
+        public virtual void DisplayInfo()
+        {
+            Console.WriteLine($"Фигура: {Name}");
+        }
+    }
+}
---
+++ Lab1_rab2_GorbatovZ.Z_BPI-23-01/Classes/Rhombus.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1_rab2_GorbatovZ.Z_BPI_23_01.Classes
+{
+    public class Rhombus : Figure
+    {
+        private double _side;
+        private double _diagonal1;
+        private double _diagonal2;
+
+        public Rhombus(double side, double diagonal1, double diagonal2)
+            : base("Ромб")
+        {
+            Side = side;
+            Diagonal1 = diagonal1;
+            Diagonal2 = diagonal2;
+        }
+
+        public double Side
+        {
+            get { return _side; }
+            set
+            {
+                if (value <= 0)
+                    throw new ArgumentException("Сторона должна быть положительным числом");
+                _side = value;
+            }
+        }
+
+        public double Diagonal1
+        {
+            get { return _diagonal1; }
+            set
+            {
+                if (value <= 0)
+                    throw new ArgumentException("Диагональ должна быть положительным числом");
+                _diagonal1 = value;
+            }
+        }
+
+        public double Diagonal2
+        {
+            get { return _diagonal2; }
+            set
+            {
+                if (value <= 0)
+                    throw new ArgumentException("Диагональ должна быть положительным числом");
+                _diagonal2 = value;
+            }
+        }
+
+  
+        public override double CalculateArea()
+        {
+            return (Diagonal1 * Diagonal2) / 2;
+        }
+
+        public override double CalculatePerimeter()
+        {
+            return 4 * Side;
+        }
+
+        public override void DisplayInfo()
+        {
+            base.DisplayInfo();
+            Console.WriteLine($"Сторона: {Side}, Диагонали: {Diagonal1} и {Diagonal2}");
+            Console.WriteLine($"Площадь: {CalculateArea():F2}");
+            Console.WriteLine($"Периметр: {CalculatePerimeter():F2}");
+        }
+    }
+}
