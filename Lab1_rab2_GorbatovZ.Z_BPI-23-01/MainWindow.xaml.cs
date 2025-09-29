49db97970dd8911c40d60ba3ff344969b319e50f
Student <Student@WKS-12-801-09>
2025-09-29 11:54:51 +0500

    Исправление с case

--- Lab1_rab2_GorbatovZ.Z_BPI-23-01/MainWindow.xaml.cs
+++ Lab1_rab2_GorbatovZ.Z_BPI-23-01/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using Lab1_rab2_GorbatovZ.Z_BPI_23_01.Classes;
 using System;
 using System.Globalization;
+using System.Linq;
 using System.Text.RegularExpressions;
 using System.Windows;
 using System.Windows.Controls;
@@ -11,11 +12,9 @@ namespace Lab1_rab2_GorbatovZ.Z_BPI_23_01
 
     public partial class MainWindow : Window
     {
-        private int figure;
         public MainWindow()
         {
             InitializeComponent();
-            figure = 0;
         }
         private void NumberValidationTextBox(object sender, TextCompositionEventArgs e)
         {
@@ -32,7 +31,7 @@ namespace Lab1_rab2_GorbatovZ.Z_BPI_23_01
             lbl2.Content = "Вторая диагональ ромба:";
             lbl3.Content = "Сторона ромба:";
             GetInfo.Visibility = Visibility.Visible;
-            figure = 0;
+     
         }
 
         private void Parallelepiped_Checked(object sender, RoutedEventArgs e)
@@ -43,7 +42,7 @@ namespace Lab1_rab2_GorbatovZ.Z_BPI_23_01
             lbl2.Content = "Ширина параллелепипеда:";
             lbl3.Content = "Высота параллелепипеда:";
             GetInfo.Visibility = Visibility.Visible;
-            figure = 1;
+
         }
 
         private void Ellipse_Checked(object sender, RoutedEventArgs e)
@@ -53,54 +52,52 @@ namespace Lab1_rab2_GorbatovZ.Z_BPI_23_01
             lbl1.Content = "Малая полуось эллипса:";
             lbl2.Content = "Большая полуось эллипса:";
             GetInfo.Visibility = Visibility.Visible;
-            figure = 2;
-
+    
         }
 
         private void GetInfo_Click(object sender, RoutedEventArgs e)
         {
             try
             {
+
+
                 string info = "";
                 string title = "";
-
-                switch (figure)
+                if (Rhombus.IsChecked.Value)
                 {
-                    case 0:
-                        double diagonal1 = double.Parse(textBox1.Text);
-                        double diagonal2 = double.Parse(textBox2.Text);
-                        double side = double.Parse(textBox3.Text);
+                    double diagonal1 = double.Parse(textBox1.Text);
+                    double diagonal2 = double.Parse(textBox2.Text);
+                    double side = double.Parse(textBox3.Text);
 
-                        Rhombus rhombus = new Rhombus(side, diagonal1, diagonal2);
-                        title = "Информация о ромбе";
-                        info = $"Сторона: {side:F2}\n" +
-                               $"Диагонали: {diagonal1:F2} и {diagonal2:F2}\n" +
-                               $"Площадь: {rhombus.CalculateArea():F2}\n" +
-                               $"Периметр: {rhombus.CalculatePerimeter():F2}";
-                        break;
+                    Rhombus rhombus = new Rhombus(side, diagonal1, diagonal2);
+                    title = "Информация о ромбе";
+                    info = $"Сторона: {side:F2}\n" +
+                           $"Диагонали: {diagonal1:F2} и {diagonal2:F2}\n" +
+                           $"Площадь: {rhombus.CalculateArea():F2}\n" +
+                           $"Периметр: {rhombus.CalculatePerimeter():F2}";
+                }
+                else if (Parallelepiped.IsChecked.Value)
+                {
+                    double length = double.Parse(textBox1.Text);
+                    double width = double.Parse(textBox2.Text);
+                    double height = double.Parse(textBox3.Text);
 
-                    case 1: 
-                        double length = double.Parse(textBox1.Text);
-                        double width = double.Parse(textBox2.Text);
-                        double height = double.Parse(textBox3.Text);
+                    Parallelepiped parallelepiped = new Parallelepiped(length, width, height);
+                    title = "Информация о параллелепипеде";
+                    info = $"Размеры: {length:F2} × {width:F2} × {height:F2}\n" +
+                           $"Площадь поверхности: {parallelepiped.CalculateArea():F2}\n" +
+                           $"Сумма длин рёбер: {parallelepiped.CalculatePerimeter():F2}\n";
+                }
+                else
+                {
+                    double semiMinor = double.Parse(textBox1.Text);
+                    double semiMajor = double.Parse(textBox2.Text);
 
-                        Parallelepiped parallelepiped = new Parallelepiped(length, width, height);
-                        title = "Информация о параллелепипеде";
-                        info = $"Размеры: {length:F2} × {width:F2} × {height:F2}\n" +
-                               $"Площадь поверхности: {parallelepiped.CalculateArea():F2}\n" +
-                               $"Сумма длин рёбер: {parallelepiped.CalculatePerimeter():F2}\n";
-                        break;
-
-                    case 2: 
-                        double semiMinor = double.Parse(textBox1.Text);
-                        double semiMajor = double.Parse(textBox2.Text);
-
-                        Ellipse ellipse = new Ellipse(semiMajor, semiMinor);
-                        title = "Информация об эллипсе";
-                        info = $"Полуоси: {semiMinor:F2} (малая) и {semiMajor:F2} (большая)\n" +
-                               $"Площадь: {ellipse.CalculateArea():F2}\n" +
-                               $"Периметр (приближённо): {ellipse.CalculatePerimeter():F2}";
-                        break;
+                    Ellipse ellipse = new Ellipse(semiMajor, semiMinor);
+                    title = "Информация об эллипсе";
+                    info = $"Полуоси: {semiMinor:F2} (малая) и {semiMajor:F2} (большая)\n" +
+                           $"Площадь: {ellipse.CalculateArea():F2}\n" +
+                           $"Периметр (приближённо): {ellipse.CalculatePerimeter():F2}";
                 }
 
                 InfoWindow infoWindow = new InfoWindow();
